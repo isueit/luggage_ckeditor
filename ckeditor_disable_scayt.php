55df79f0c327c90f25acdbad0cbd20a94ba25173
John Rearick <jrearick@iastate.edu>
2020-03-06 16:30:19 -0600

    LUGG-1194 Update instructions for ckeditor_disable_scayt.php

--- ckeditor_disable_scayt.php
+++ ckeditor_disable_scayt.php
@@ -4,8 +4,8 @@
 
 This is inteded to quickly disable the Spell Check as You Type (scayt) plugin in Luggage's
 WYSIWYG editor if you are not able to update Luggage. The Luggage update includes a
-hook_update() that runs similar code. Use this only if you are not upgradeing Luaggage
-itself. By default the scayt will display ads prompting user to buy the service. This
+hook_update() that runs similar code. Use this only if you are not upgrading Luaggage
+itself. By default the scayt will display ads prompting the user to buy the service. This
 shoudn't override any other CKEditor settings and should work with even if the feature is
 overriden.
 
@@ -13,10 +13,20 @@ Please test this locally before running on live.
 
 To use:
 
-# cd /path/to/your/luggage/site
-# cd sites/all/modules/luggage/luggage_ckeditor # Where you find this file.
+Download the script to where ever you want (curl -O or wget):
+# cd /tmp
+# curl -O https://raw.githubusercontent.com/isubit/luggage_ckeditor/master/ckeditor_disable_scayt.php
 
-# drush eval "include('ckeditor_disable_scayt.php');"
+Run it on your site:
+# cd /path/to/your/luggage/site
+# drush eval "include('/tmp/ckeditor_disable_scayt.php');"
+# drush cc all
+
+Check to make sure it applied properly. If you have other sites on the server, you can
+skip the downloading part and just run it on each site. When you are done with all the
+sites, you may remove the script.
+
+# rm /tmp/ckeditor_disable_scayt.php
 
 **/
 
